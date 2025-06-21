2f0ee494253ac9d071256c5f36957726605bdd33
Matteo Fonsatti <matteo.fonsatti@hudl.com>
2025-06-21 17:10:55 +0200

    Add trip-search module to the gateway

--- apps/trip-api-gateway/src/trip-api-gateway.module.ts
+++ apps/trip-api-gateway/src/trip-api-gateway.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TripApiGatewayController } from './trip-api-gateway.controller';
 import { TripApiGatewayService } from './trip-api-gateway.service';
+import { TripSearchModule } from './trip-search/trip-search.module';
 
 @Module({
-  imports: [],
+  imports: [TripSearchModule],
   controllers: [TripApiGatewayController],
   providers: [TripApiGatewayService],
 })
