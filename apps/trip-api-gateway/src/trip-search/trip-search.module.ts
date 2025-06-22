441aa76aceb03d38b958716569f0032751d35204
Matteo Fonsatti <matteo.fonsatti@hudl.com>
2025-06-22 12:31:30 +0200

    Connected the gateway to the microservice with Message Pattern via TCP

--- apps/trip-api-gateway/src/trip-search/trip-search.module.ts
+++ apps/trip-api-gateway/src/trip-search/trip-search.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { TripSearchService } from './trip-search.service';
 import { TripSearchController } from './trip-search.controller';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'TRIP_SEARCH_CLIENT',
+        transport: Transport.TCP,
+        options: {
+          port: 3001,
+        },
+      },
+    ]),
+  ],
   providers: [TripSearchService],
-  controllers: [TripSearchController]
+  controllers: [TripSearchController],
 })
 export class TripSearchModule {}
--- apps/trip-api-gateway/src/trip-search/trip-search.service.ts
+++ apps/trip-api-gateway/src/trip-search/trip-search.service.ts
@@ -1,8 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
 
 @Injectable()
 export class TripSearchService {
+  constructor(
+    @Inject('TRIP_SEARCH_CLIENT') private tripSearchClient: ClientProxy,
+  ) {}
   search() {
-    return 'mock search response';
+    //return 'mock search response from the gateway';
+    return this.tripSearchClient.send('trip.search', {});
   }
 }
