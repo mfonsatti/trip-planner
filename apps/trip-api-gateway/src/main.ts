bf1437830f221e8ce5b275ca85fbbe7ea0eb84aa
Matteo Fonsatti <matteo.fonsatti@hudl.com>
2025-06-23 12:45:56 +0200

    Added validation and set sort_by as optional

--- apps/trip-api-gateway/src/main.ts
+++ apps/trip-api-gateway/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { TripApiGatewayModule } from './trip-api-gateway.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(TripApiGatewayModule);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   await app.listen(process.env.port ?? 3000);
 }
 bootstrap();
--- apps/trip-search/src/dto/search.dto.ts
+++ apps/trip-search/src/dto/search.dto.ts
@@ -1,20 +1,85 @@
+import { IsEnum, IsIn, IsOptional, IsString } from 'class-validator';
+
 export enum SortBy {
   FASTEST = 'fastest',
   CHEAPEST = 'cheapest',
 }
 
+export const SupportedIATA = [
+  'ATL',
+  'PEK',
+  'LAX',
+  'DXB',
+  'HND',
+  'ORD',
+  'LHR',
+  'PVG',
+  'CDG',
+  'DFW',
+  'AMS',
+  'FRA',
+  'IST',
+  'CAN',
+  'JFK',
+  'SIN',
+  'DEN',
+  'ICN',
+  'BKK',
+  'SFO',
+  'LAS',
+  'CLT',
+  'MIA',
+  'KUL',
+  'SEA',
+  'MUC',
+  'EWR',
+  'MAD',
+  'HKG',
+  'MCO',
+  'PHX',
+  'IAH',
+  'SYD',
+  'MEL',
+  'GRU',
+  'YYZ',
+  'LGW',
+  'BCN',
+  'MAN',
+  'BOM',
+  'DEL',
+  'ZRH',
+  'SVO',
+  'DME',
+  'JNB',
+  'ARN',
+  'OSL',
+  'CPH',
+  'HEL',
+  'VIE',
+] as const;
+
+export type IATACode = (typeof SupportedIATA)[number];
+
 export class SearchQueryDto {
-  origin: string;
-  destination: string;
-  sort_by: SortBy;
+  @IsString()
+  @IsIn(SupportedIATA)
+  origin: IATACode;
+
+  @IsString()
+  @IsIn(SupportedIATA)
+  destination: IATACode;
+
+  @IsOptional()
+  @IsEnum(SortBy)
+  sort_by?: SortBy;
 }
 
 export class TripDto {
-    origin: string
-    destination: string
-    cost: number
-    duration: number
-    type: string
-    id: string
-    display_name: string
+  origin: string;
+  destination: string;
+  cost: number;
+  duration: number;
+  type: string;
+  id: string;
+  display_name: string;
 }
